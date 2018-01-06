abf84346e4a0f51bd2dca5eeea98369a091001d9
19648 <1964855183@qq.com>
2018-01-06 13:44:23 +0800

    User can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 Route::resource('projects', 'ProjectsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+//话题发布页面图片上传功能路由
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
