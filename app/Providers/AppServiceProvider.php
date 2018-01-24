33d8be52695f21d8e092cf329e24ba485676a16c
19648 <1964855183@qq.com>
2018-01-24 15:06:18 +0800

    Sidebar links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,11 +18,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        Schema::defaultStringLength(191);
+        \App\Models\User::observe(\App\Observers\UserObserver::class);
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+        \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+
+        // Carbon 中文化配置
         \Carbon\Carbon::setLocale('zh');
-        Topic::observe(TopicObserver::class);
-        Reply::observe(ReplyObserver::class);
     }
 
     /**
