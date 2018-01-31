eeecc96d1ab16dcfe0f1f0c2bd2632886f24f8b5
19648 <1964855183@qq.com>
2018-01-31 11:20:19 +0800

    api/user

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\UserRequest;
+use App\Transformers\UserTransformer;
 
 class UsersController extends Controller
 {
@@ -31,5 +32,16 @@ class UsersController extends Controller
         \Cache::forget($request->verification_key);
 
         return $this->response->created();
+    }
+
+    public function me()
+    {
+        return $this->response->item($this->user(), new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($this->user()),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
     }
 }
